b4bbf2cf50178118188d31cc52f0651c9b947b64
Yasshharad <it.a.35.yasshharad@gmail.com>
2022-09-10 10:59:27 +0530

    added new routes for user

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { users } = require("./data/users.json");
 
 const app = express();
 
@@ -12,11 +13,83 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/users', (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: users,
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = users.find((each) => each.id === id);
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found",
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        data: user,
+    })
+});
+
+app.post('/users', (req, res) => {
+    const { id, name, surname, email, subscriptionType, subscriptionDate } = req.body;
+    const user = users.find((each) => each.id === id);
+    if (user) {
+        return res.status(404).json({
+            success: false,
+            message: "User exists with this id",
+        });
+    }
+    users.push({
+        id,
+        name,
+        surname,
+        email,
+        subscriptionType,
+        subscriptionDate,
+    });
+
+    return res.status(201).json({
+        success: true,
+        data: users,
+    })
+});
+
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { data } = req.body;
+
+    const user = users.find((each) => each.id === id);
+
+    if (!user)
+        return res.status(404).json({ success: false, message: "User not found" });
+
+    const updatedUser = users.map((each) => {
+        if (each.id === id) {
+            return {
+                ...each,
+                ...data,
+            };
+        }
+        return each;
+    });
+
+    return res.status(200).json({
+        success: true,
+        data: updatedUser,
+    });
+});
+
 app.get('*', (req, res) => {
     res.status(404).json({
         message: "This route does not exist"
-    })
-})
+    });
+});
+
 
 
 app.listen(PORT, () => {
