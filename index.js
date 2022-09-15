661d3d35246615f6ec3970888db24ecbd4d11d5d
Yasshharad <it.a.35.yasshharad@gmail.com>
2022-09-15 23:16:47 +0530

    refactored code and added some books routes

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+//JSON data input
 const { users } = require("./data/users.json");
+//importing routes
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
+
 
 const app = express();
 
@@ -7,82 +13,14 @@ const PORT = 8081;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.status(200).json({
         message: "Server is up and running",
     });
 });
 
-app.get('/users', (req, res) => {
-    res.status(200).json({
-        success: true,
-        data: users,
-    });
-});
-
-app.get('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const user = users.find((each) => each.id === id);
-    if (!user) {
-        return res.status(404).json({
-            success: false,
-            message: "User not found",
-        });
-    }
-    return res.status(200).json({
-        success: true,
-        data: user,
-    })
-});
-
-app.post('/users', (req, res) => {
-    const { id, name, surname, email, subscriptionType, subscriptionDate } = req.body;
-    const user = users.find((each) => each.id === id);
-    if (user) {
-        return res.status(404).json({
-            success: false,
-            message: "User exists with this id",
-        });
-    }
-    users.push({
-        id,
-        name,
-        surname,
-        email,
-        subscriptionType,
-        subscriptionDate,
-    });
-
-    return res.status(201).json({
-        success: true,
-        data: users,
-    })
-});
-
-app.put('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const { data } = req.body;
-
-    const user = users.find((each) => each.id === id);
-
-    if (!user)
-        return res.status(404).json({ success: false, message: "User not found" });
-
-    const updatedUser = users.map((each) => {
-        if (each.id === id) {
-            return {
-                ...each,
-                ...data,
-            };
-        }
-        return each;
-    });
-
-    return res.status(200).json({
-        success: true,
-        data: updatedUser,
-    });
-});
+app.use('/users', usersRouter);
+app.use('/books', booksRouter);
 
 app.get('*', (req, res) => {
     res.status(404).json({
---
+++ routes/books.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const { books } = require("../data/books.json");
+const { users } = require("../data/users.json");
+
+const router = express.Router();
+
+//get all the books
+router.get('/', (req, res) => {
+    res.status(200).json({
+        success: true, data: books
+    });
+})
+
+//get all the books by id
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    const book = books.find((each) => each.id === id);
+    if (!book) {
+        return res.status(404).json({
+            success: false,
+            message: "Book not found",
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        data: book,
+    });
+});
+
+// get all issued books
+router.get("/issued/by-user", (req, res) => {
+    const usersWithIssuedBooks = users.filter((each) => {
+        if (each.issuedBook) return each;
+    });
+
+    const issuedBooks = [];
+
+    usersWithIssuedBooks.forEach((each) => {
+        const book = books.find((book) => book.id === each.issuedBook);
+
+        book.issuedBy = each.name;
+        book.issuedDate = each.issuedDate;
+        book.returnDate = each.returnDate;
+
+        issuedBooks.push(book);
+    });
+
+    if (issuedBooks.length === 0)
+        return res.status(404).json({
+            success: false,
+            message: "No books issued yet",
+        });
+
+    return res.status(200).json({
+        success: true,
+        data: issuedBooks,
+    });
+});
+
+
+module.exports = router;
