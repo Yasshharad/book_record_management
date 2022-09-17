847e6aeb024bb7ef872e4e128289a3439992b8e9
Yasshharad <it.a.35.yasshharad@gmail.com>
2022-09-17 11:33:21 +0530

    created connection with mongoDB and added dotenv file and data

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
-//JSON data input
-const { users } = require("./data/users.json");
+const dotenv = require("dotenv");
+//database connection
+const DbConnection = require("./databaseConnection");
 //importing routes
 const usersRouter = require("./routes/users");
 const booksRouter = require("./routes/books");
 
-
+dotenv.config();
 
 const app = express();
+
+DbConnection();
 
 const PORT = 8081;
 
--- routes/books.js
+++ routes/books.js
@@ -57,5 +57,60 @@ router.get("/issued/by-user", (req, res) => {
     });
 });
 
+//create new book
+router.post("/", (req, res) => {
+    const { data } = req.body;
 
+    if (!data) {
+        return res.status(400).json({
+            success: false,
+            message: "No data provided",
+        });
+    }
+
+    const book = books.find((each) => each.id === data.id);
+
+    if (book) {
+        return res.status(404).json({
+            success: false,
+            message: "Book already exists with this id, please use a unique id",
+        });
+    }
+
+    const allBooks = [...books, data];
+
+    return res.status(201).json({
+        success: true,
+        data: allBooks,
+    });
+});
+
+//update  book
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { data } = req.body;
+
+    const book = books.find((each) => each.id === id);
+
+    if (!book) {
+        return res.status(400).json({
+            success: false,
+            message: "Book not found with this particular id",
+        });
+    }
+
+    const updateData = books.map((each) => {
+        if (each.id === id) {
+            return { ...each, ...data };
+        }
+        return each;
+    });
+
+    return res.status(200).json({
+        success: true,
+        data: updateData,
+    });
+});
+
+//get all issued books with fine
 module.exports = router;
